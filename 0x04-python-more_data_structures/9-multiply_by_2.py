9efe4ddc347323d647f08e6f2179e5592bd469fa
CindyMuraya <cmuraya12@gmail.com>
2022-07-06 16:45:25 +0000

    task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """returns a new dictionary with all values multiplied by 2"""
+
+    return ({i: a_dictionary[i] * 2 for i in a_dictionary})
