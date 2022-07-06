a2769b2461b33f24b60f5fc5cb7d8ef52b807045
CindyMuraya <cmuraya12@gmail.com>
2022-07-06 17:32:36 +0000

    task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """returns the weighted average of all integers tuple"""
+    if not my_list:
+        return 0
+
+    average = 0
+    div = 0
+
+    for tup in my_list:
+        average += tup[0] * tup[1]
+        div += tup[1]
+
+    return float(average / div)
