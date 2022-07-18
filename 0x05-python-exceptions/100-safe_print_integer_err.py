bcf05c853bb976bb0923d32b42cb9f7b528a2cc7
CindyMuraya <cmuraya12@gmail.com>
2022-07-18 17:57:37 +0000

    task 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format().
+
+    Args:
+        value (int): integer to print; can be any type
+
+    Returns:
+        True if value has been correctly printed - the value is an integer
+        Otherwise - False and prints in stderr the error precede by Exception:
+    """
+
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
