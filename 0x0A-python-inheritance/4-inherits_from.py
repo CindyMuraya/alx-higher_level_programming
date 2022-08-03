1b41a3e11f411f513c1bca67525428a5777782be
CindyMuraya <cmuraya12@gmail.com>
2022-08-03 13:18:12 +0000

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""defines a function that checks if an onject is inherited"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class
+
+    Args:
+        obj (any): object to check
+        a_class (type): the class to match the type of obj to
+
+    Returns:
+        True if the object is an instance of a class that inherited from the specified class
+        Otherwise - False.
+    """
+
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
